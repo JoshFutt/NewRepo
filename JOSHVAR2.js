0d0ca2208641d5ec57d906dd4c9d37dad52274c0
Josh Futt <YOUR@EMAIL.com>
2016-02-12 16:29:00 -0500

    Homework

---
+++ JOSHVAR2.js
@@ -0,0 +1,28 @@
+var ten = 10;
+var twenty = 20;
+var thirty = 30;
+
+var ADD = ten + twenty;
+var MINUS = twenty - ten;
+var MULTIPLY = twenty * ten;
+var DIVIDE = twenty / ten;
+var PLUS = ten + twenty + thirty;
+
+
+
+document.getElementById("ADD").innerHTML = " I will add " + ten + " With " + twenty + " and it will be " + ADD + ".";
+document.getElementById("ADD").style.color="Pink";
+document.getElementById("ADD").style.fontFamily="Courier";
+
+document.getElementById("MINUS").innerHTML = "When I MINUS " + twenty + " from " + ten + " I get " + MINUS + ".";
+document.getElementById("MINUS").style.color= "Orange";
+document.getElementById("MINUS").style.fontFamily="Times New Roman";
+
+document.getElementById("MULTIPLY").innerHTML = "When I MULTIPLY " + twenty + " by " + ten + " I get " + MULTIPLY + ".";
+document.getElementById("MULTIPLY").style.color= "blue";
+document.getElementById("MULTIPLY").style.fontFamily="Arial"
+
+document.getElementById("DIVIDE").innerHTML = "When I DIVIDE " + twenty + " by " + ten + " it is " + DIVIDE + ".";
+document.getElementById("DIVIDE").style.color= "red"; 
+document.getElementById("DIVIDE").style.fontFamily= "Verdana";
+
